5eaa39b67354d13d98c0850791a1c88c49eef0b7
nickilanjelo <nikita.ilyasoff@gmail.com>
2021-02-26 16:10:25 +0300

    started new codelab

--- app/src/main/java/com/nickilanjelo/composeapp/MainActivity.kt
+++ app/src/main/java/com/nickilanjelo/composeapp/MainActivity.kt
@@ -6,19 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nickilanjelo.composeapp.ui.theme.ComposeAppTheme
@@ -27,82 +25,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp{
-                MyScreenContent()
+
+        }
+    }
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Surface(color = Color.White) {
+        Row(modifier
+            .padding(16.dp)
+            .clickable { /* Ignore */ }
+        ) {
+            Surface(
+                modifier = Modifier.size(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+            ) {
+                // Image
+            }
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+                }
             }
         }
     }
 }
 
-@Composable
-fun MyApp(content: @Composable () -> Unit) {
-    ComposeAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = Color.Yellow) {
-            content()
-        }
-    }
-}
-
-@Composable
-fun MyScreenContent(names: List<String> = List(1000) { "Hello Android $it" }) {
-
-    val counterState = remember(calculation = { mutableStateOf(0) })
-
-    Column(modifier = Modifier.fillMaxHeight()) {
-        NameList(names, Modifier.weight(1f))
-        Counter(
-            count = counterState.value,
-            updateCount = { newCount ->
-                counterState.value = newCount
-            }
-        )
-    }
-}
-
-@Composable
-fun NameList(names: List<String>, modifier: Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(items = names) { name ->
-            Greeting(name = name)
-            Divider(color = Color.Black)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    var isSelected: Boolean by remember { mutableStateOf(false) }
-    val backgroundColor by animateColorAsState(if (isSelected) Color.Red else Color.Transparent)
-
-    Text(
-        text = "Hello $name!",
-        modifier = Modifier
-            .padding(28.dp)
-            .background(color = backgroundColor)
-            .clickable {
-                isSelected = !isSelected
-            },
-        style = MaterialTheme.typography.h1
-    )
-}
-
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(
-        onClick = { updateCount(count + 1) },
-        colors = ButtonDefaults.buttonColors(
-            backgroundColor = if (count < 5) Color.White else Color.Green
-        )
-    ) {
-        Text(text = "I've been clicked $count times")
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApp {
-        MyScreenContent()
+    ComposeAppTheme {
+        PhotographerCard()
     }
 }
